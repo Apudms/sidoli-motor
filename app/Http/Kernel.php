d400b06c7cd16558d60658b5c358a86b54040bb9
Muhammad Saefudin <itsapud@gmail.com>
2023-05-31 02:37:33 +0700

    Add AuthAdmin

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,6 +54,8 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
+        'authowner' => \App\Http\Middleware\Authenticate::class,
+        'authadmin' => \App\Http\Middleware\Authenticate::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
