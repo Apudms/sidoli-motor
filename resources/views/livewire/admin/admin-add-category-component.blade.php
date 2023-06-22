0ca227f8112b90a7314805dd55901332231467ee
Muhammad Saefudin <itsapud@gmail.com>
2023-06-23 04:09:34 +0700

    Add "AllCategories" & "AddNewCategory" Page

---
+++ resources/views/livewire/admin/admin-add-category-component.blade.php
@@ -0,0 +1,47 @@
+<div>
+    <div class="container" style="padding: 30px 0;">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-md-6">
+                                Tambah Kategori Baru
+                            </div>
+                            <div class="col-md-6">
+                                <a href="{{ route('admin.kategori') }}" class="btn btn-success pull-right">Semua
+                                    Kategory</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        @if (Session::has('message'))
+                        <div class="alert alert-success" role="alert">{{ Session::get('message') }}</div>
+                        @endif
+                        <form class="form-horizontal" wire:submit.prevent="storeCategory">
+                            <div class="form-group">
+                                <label for="" class="col-md-4 control-label">Nama Kategori</label>
+                                <div class="col-md-4">
+                                    <input type="text" name="" id="" class="form-control input-md"
+                                        placeholder="Nama Kategori" wire:model="name" wire:keyup="generateslug">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="" class="col-md-4 control-label">Slug Kategori</label>
+                                <div class="col-md-4">
+                                    <input type="text" name="" id="" class="form-control input-md"
+                                        placeholder="Slug Kategori" disabled readonly wire:model="slug">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="" class="col-md-4 control-label">
+                                    <button type="submit" class="btn btn-primary">Simpan</button>
+                                </label>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
